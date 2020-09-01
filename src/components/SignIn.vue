b1afe195c06626daabcc128c7633374802f833ab
xeyzo <schneidergw@yahoo.com>
2020-09-01 21:43:26 +0700

    add sign in

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -6,11 +6,11 @@
 <form class="w-full max-w-lg">
   <div class="flex flex-wrap -mx-3 mb-6">
     <div class="w-full px-3 mb-6 md:mb-0 ml-8 mr-8">
-    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="fullname">
-    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="username">
-    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************">
-    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="phone number">
-    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="example@mail.com">
+    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white " v-model="full_name" id="grid-first-name" type="text" placeholder="fullname">
+    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" v-model="username" id="grid-first-name" type="text" placeholder="username">
+    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" v-model="password" id="grid-password" type="password" placeholder="******************">
+    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" v-model="phone_number" type="text" placeholder="phone number">
+    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" v-model="email" type="text" placeholder="example@mail.com">
     <div class="flex items-center justify-between">
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="button">
         Sign In
@@ -26,6 +26,15 @@
 
 <script>
 export default {
-    name:"SignIn",
+  name:"SignIn",
+  data(){
+  return{  
+      full_name:"",
+      username:"",
+      email:"",
+      phone_number:"",
+      password:""
+    }
+  }
 }
 </script>
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,10 @@
-import Home from "./pages/Home.vue";
 import SignIn from "./components/SignIn.vue";
 import Login from "./components/Login.vue";
 
 
 const routes = [
-  { path: "/", component: Home },
   { path: "/SignIn", component: SignIn },
   { path: "/Login", component: Login }
-  //   { path: "/user/:id", component: UserDetail },
 ];
 
 export default routes;
